bc5ca11668b1223f7f9448bee345465defbda20d
Yasir Akbar <160271191+yasir2244@users.noreply.github.com>
2024-03-26 19:46:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("wellcome to CodeWithyasir - ClI Number Guessing Game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "usrGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.usrGuessedNumber === randomNumber) {
+    console.log("congratulation ! you guess a correct number.");
+}
+else {
+    console.log("sory, you guess a wring number");
+}
